77dc55a22c2e0f09293766690e7c9101f6807b10
Nothingbro1 <148611363+Nothingbro1@users.noreply.github.com>
2023-10-21 19:56:16 +0530

    Update PancakePair.sol
    To Avoid Errors

--- projects/exchange-protocol/contracts/PancakePair.sol
+++ projects/exchange-protocol/contracts/PancakePair.sol
@@ -176,7 +176,6 @@ contract PancakePair is IPancakePair, PancakeERC20 {
         }
         uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
-        require(amount0In > 0 || amount1In > 0, 'Pancake: INSUFFICIENT_INPUT_AMOUNT');
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
         uint balance0Adjusted = (balance0.mul(10000).sub(amount0In.mul(25)));
         uint balance1Adjusted = (balance1.mul(10000).sub(amount1In.mul(25)));
