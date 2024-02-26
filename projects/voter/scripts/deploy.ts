bd4b99af1f11aa9bcf5cfe1b74281cbd98e040c8
ChefMist <mist@pancakeswap.com>
2024-02-26 15:54:12 +0800

    ci: Fix prettifier warning

--- projects/voter/scripts/deploy.ts
+++ projects/voter/scripts/deploy.ts
@@ -7,7 +7,7 @@ const main = async () => {
 
   let ContractObj = await ethers.getContractFactory("GaugeVoting");
   let obj = await ContractObj.deploy(
-      '0x5692DB8177a81A6c6afc8084C2976C9933EC1bAB', // veCake
+    "0x5692DB8177a81A6c6afc8084C2976C9933EC1bAB" // veCake
   );
   await obj.deployed();
   console.log("GaugeVoting deployed to:", obj.address);
@@ -22,6 +22,7 @@ const main = async () => {
   await obj.deployed();
   console.log("GaugeVotingCalc deployed to:", obj.address);
 };
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
